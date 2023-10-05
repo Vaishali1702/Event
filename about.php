ddc66a339d29cf2e83509ee61c5a2b6a18edc7e8
Vaishali1702 <127035340+Vaishali1702@users.noreply.github.com>
2023-10-05 00:53:27 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Event Organizer Website Design</title>
+    <!-- Font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <!-- Custom css file link -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<!-- Header section starts -->
+<header class="header">
+    <a href="#" class="logo"><span>L</span>uxurious</a>
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#service">service</a>
+        <a href="#about">about</a>
+        <a href="#gallery">gallery</a>
+        <a href="#price">price</a>
+        <a href="#review">review</a>
+        <a href="#contact">contact</a>
+    </nav>
+
+    <div id="menu-bars" class="fas fa-bars"></div>
+</header>
+<!-- Header section ends -->
+
+<!-- About section starts -->
+
+<section class="about" id="about">
+
+  <h1 class="heading"><span>About</span> US </h1>
+
+  <div class="row">
+
+    <div class="image">
+      <img src="images/11.jpg" height="550px" width="800px" alt="">
+    </div>
+
+    <div class="content">
+      <h3>We Will Planned a Unique luxurious Events and Celebrations for you</h3>
+      <a href="#" class="btn">contact us</a>
+    </div>
+  </div>
+
+</section>
+<!-- About section ends -->
+
+<!-- Footer section starts -->
+<section class="footer">
+  <div class="box-container">
+    <div class="box">
+      <h3>Branches</h3>
+      <a href="#"> <i class="fas fa-map-marker-alt"></i> Bangaluru </a>
+      <a href="#"> <i class="fas fa-map-marker-alt"></i> Kolkata </a>
+      <a href="#"> <i class="fas fa-map-marker-alt"></i> Goa </a>
+      <a href="#"> <i class="fas fa-map-marker-alt"></i> Mumbai </a>
+      <a href="#"> <i class="fas fa-map-marker-alt"></i> Ahmedabad </a>
+    </div>
+
+
+    <div class="box">
+      <h3>Quick links</h3>
+      <a href="#"> <i class="fas fa-arrow-right"></i> Home </a>
+      <a href="#"> <i class="fas fa-arrow-right"></i> service </a>
+      <a href="#"> <i class="fas fa-arrow-right"></i> about </a>
+      <a href="#"> <i class="fas fa-arrow-right"></i> Gallery </a>
+      <a href="#"> <i class="fas fa-arrow-right"></i> price </a>
+      <a href="#"> <i class="fas fa-arrow-right"></i> review </a>
+       <a href="#"> <i class="fas fa-arrow-right"></i> Contact </a>
+      </div>
+
+
+      <div class="box">
+        <h3>Contact information</h3>
+        <a href="#"> <i class="fas fa-phone"></i> 8276497634 </a>
+        <a href="#"> <i class="fas fa-phone"></i> 8797649554 </a>
+        <a href="#"> <i class="fas fa-envelope"></i> guptaanaisha@gmail.com </a>
+        <a href="#"> <i class="fas fa-envelope"></i> guptaivansha@gmail.com </a>
+        <a href="#"> <i class="fas fa-map-marker-alt"></i> Bangaluru, India - 5900156 </a>
+      </div>
+
+
+      <div class="box">
+        <h3>Follow us</h3>
+        <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>  
+        <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+        <a href="#"> <i class="fab fa-linkedin"></i> Linkedin </a>
+        <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+        </div>
+
+
+
+  </div>
+  <div class="credit"> created by <span>Miss Vaishali</span> | All rights reserved  </div>
+</section>
+<!-- Footer section ends -->
+
+
+
+
+
+
+    <!-- Custom js file link -->
+    <script src="script.js"></script>
+</body>
+</html>
